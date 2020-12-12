8a6c9dc0784b6629900f8eb87df7a588536542ae
Mahendra Gulla <gullamahi@gmail.com>
2020-12-13 00:37:43 +0100

    internationalization

--- Project1/webapp/controller/App.controller.js
+++ Project1/webapp/controller/App.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict"
     return Controller.extend("sap.ui.demo.walkthrough.App", {
         onInit: function () {
@@ -11,11 +12,22 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
                 }
             };
             var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel );
+            this.getView().setModel(oModel);
+            // i18n model
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
 
         onShowHello: function () {
-            MessageToast.show("Hello There!");
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            //var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            MessageToast.show(sMsg);
         }
 
     })
