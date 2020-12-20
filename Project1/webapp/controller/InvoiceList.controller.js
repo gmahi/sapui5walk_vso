2410f852ad7bddd91e22aa3e7b3be404a6f3876e
Mahendra Gulla <gullamahi@gmail.com>
2020-12-20 14:56:46 +0100

    Filter

--- Project1/webapp/controller/InvoiceList.controller.js
+++ Project1/webapp/controller/InvoiceList.controller.js
@@ -3,19 +3,37 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
   ],
-  function (Controller, JSONModel, formatter) {
+  function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
-     formatter:formatter,
-     
-     
+      formatter: formatter,
+
       onInit: function () {
         var oViewModel = new JSONModel({
           currency: "EUR",
         });
 
         this.getView().setModel(oViewModel, "view");
+      },
+
+      onFilterInvoices: function (oEvent) {
+        //build filter Array
+
+        var aFilter = [];
+        var sQuery = oEvent.getParameter("query");
+        if (sQuery) {
+          aFilter.push(
+            new Filter("ProductName", FilterOperator.Contains, sQuery)
+          );
+        }
+
+        // filter Binding
+         var oList = this.byId("invoiceList");
+         var oBinding = oList.getBinding("items");
+         oBinding.filter( aFilter );
       },
     });
   }
